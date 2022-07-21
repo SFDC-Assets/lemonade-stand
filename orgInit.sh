0fee624675007ad48a2d9a8377cef6102cf56678
Jonathan Zhou <34146693+zhou059@users.noreply.github.com>
2022-07-21 11:45:58 -0700

    Update orgInit.sh

--- orgInit.sh
+++ orgInit.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-sfdx shane:org:create -f config/project-scratch-def.json -d 5 -s --wait 60 --userprefix lemonade -o stand.done
+sfdx shane:org:create -f config/project-scratch-def.json -d 30 -s --wait 60 --userprefix lemonade -o stand.done
 sfdx force:source:push
 sfdx shane:user:password:set -g User -l User -p salesforce1
 sfdx force:org:open
